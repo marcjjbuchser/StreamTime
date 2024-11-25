59f4562737033e4b52213bccbdb401222fc7e418
Marc Buchser <124202951+marcjjbuchser@users.noreply.github.com>
2024-11-25 13:30:22 -0700

    Initial Commit

---
+++ StreamTime/StreamTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTimeApp.swift
+//  StreamTime
+//
+//  Created by Marc Buchser on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct StreamTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamTimeTests/StreamTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTimeTests.swift
+//  StreamTimeTests
+//
+//  Created by Marc Buchser on 2024-11-25.
+//
+
+import Testing
+@testable import StreamTime
+
+struct StreamTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
